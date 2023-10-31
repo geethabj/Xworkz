0b7987f49841041c1dc6b3dba07fc009f38c43a2
geethabj <geetha.xworkz@gmail.com>
2023-11-01 04:04:35 +0530

    rocket instance added

--- src/Instancevariable/Allclassmain.java
+++ src/Instancevariable/Allclassmain.java
@@ -5,10 +5,16 @@ public class Allclassmain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Clock clock = new Clock("indian",24.0,"IST","glass");
-		System.out.println("Matchbox brand is : " + clock.Timezone);
-		System.out.println("Matchbox length : " + clock.Hours);
-		System.out.println("Matchbox NoOfSticks are : " + clock.type);
-		System.out.println("Matchbox price is : " + clock.material);
+		System.out.println("Timezone : " + clock.Timezone);
+		System.out.println("Hours : " + clock.Hours);
+		System.out.println("type" + clock.type);
+		System.out.println("material" + clock.material);
+		System.out.println("\n");
+		Rocket rocket = new Rocket(200,50,"white", "airindia");
+		System.out.println("No of seats in Rocket: " + rocket.Noofseats);
+		System.out.println("No of windwos in Rocket" + rocket.Noofwindwos);
+		System.out.println("colour : " + rocket.colour);
+		System.out.println("campany : " + rocket.company);
 		System.out.println("\n");
 	}
 
